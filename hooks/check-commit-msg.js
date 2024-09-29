cdb2c8bda82086dd6fecb5602c11dab833a15d23
xuzhenhui <zhenhui.xu@thoughtworks.com>
2024-09-29 16:14:09 +0800

    N/A feat: support pass types via arg

--- hooks/check-commit-msg.js
+++ hooks/check-commit-msg.js
@@ -1,43 +1,49 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const { program } = require('commander');
 
 const TICKET_PREFIX = ['TO', 'ECS', 'P20E'];
 
 const COMMIT_TYPE = [
-    'build',
-    'chore',
-    'ci',
-    'docs',
-    'feat',
-    'fix',
-    'perf',
-    'refactor',
-    'revert',
-    'style',
-    'test',
+  'build',
+  'chore',
+  'ci',
+  'docs',
+  'feat',
+  'fix',
+  'perf',
+  'refactor',
+  'revert',
+  'style',
+  'test',
 ];
 
-const commitMsgFile = process.argv[2];
+program.argument('[string...]').parse();
+
+const args = program.args;
+
+const commitMsgFile = args.pop();
+const supportedTypes = args.length === 0 ? COMMIT_TYPE : args;
 
 const commitMessage = fs.readFileSync(commitMsgFile, 'utf-8').trim();
 
 const ticketPattern = `(((${TICKET_PREFIX.join('|')})-\\d+)|N/A)`;
-const typePattern = `(${COMMIT_TYPE.join('|')})`;
+const typePattern = `(${supportedTypes.join('|')})`;
 
 const regex = new RegExp(`^${ticketPattern} ${typePattern}: .+$`);
 
 const result = regex.test(commitMessage);
 
 if (!result) {
-    console.log(
-        `
+  console.log(
+    `
         Invalid commit message. please follow this template:   
             ticket type: message(TO-123 feat: message).
         Ticket: ${TICKET_PREFIX.join(', ')}
-        Type: ${COMMIT_TYPE.join(', ')}
+        Type: ${supportedTypes.join(', ')}
         `
-    );
+  );
 }
 
 process.exit(regex.test(commitMessage) ? 0 : 1);
