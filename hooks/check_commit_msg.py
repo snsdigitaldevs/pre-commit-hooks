b9bb5343657586761aac393f236aec80434c24fd
xuzhenhui <zhenhui.xu@thoughtworks.com>
2024-12-16 11:12:51 +0800

    N/A feat: add CRS ticket prefix

--- hooks/check_commit_msg.py
+++ hooks/check_commit_msg.py
@@ -13,7 +13,7 @@ class Colors:
 RESULT_SUCCESS = 0
 RESULT_FAIL = 1
 
-TICKET_PREFIX = ["TO", "ECS", "P20E"]
+TICKET_PREFIX = ["TO", "ECS", "P20E", "CRS"]
 
 SPECIAL_PREFIX = ["Merge branch", "Revert"]
 
