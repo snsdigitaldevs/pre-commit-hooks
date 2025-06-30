a6129d736a77e4ae8dcd25398f02b111da303120
Hao Zhu <hao.zhu@thoughtworks.com>
2025-06-30 10:47:47 +0800

    N/A feat: add ticket type RNUG

--- hooks/check_commit_msg.py
+++ hooks/check_commit_msg.py
@@ -13,7 +13,7 @@ class Colors:
 RESULT_SUCCESS = 0
 RESULT_FAIL = 1
 
-TICKET_PREFIX = ["TO", "ECS", "P20E", "CRS", "VR"]
+TICKET_PREFIX = ["TO", "ECS", "P20E", "CRS", "VR", "RNUG"]
 
 SPECIAL_PREFIX = ["Merge branch", "Revert"]
 
